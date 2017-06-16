58f5ba0c59f394710eb2263c90f68011eac51a03
Pablo Peralta <cellosofia1@gmail.com>
2017-06-16 23:53:07 +0100

    .

--- ProyectoSysTurismo/frmPrincipal.Designer.vb
+++ ProyectoSysTurismo/frmPrincipal.Designer.vb
@@ -279,6 +279,7 @@ Partial Class frmPrincipal
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(660, 369)
         Me.Controls.Add(Me.tlsPrincipal)
         Me.Controls.Add(Me.stsPrincipal)
